65815cf92d1eadb0cf76ca22ba2886e527c2c4fa
nroxa92 <nevenroksa@gmail.com>
2025-10-02 09:04:44 +0200

    s

--- mobile_app/android/app/src/main/kotlin/com/example/mobile_app/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/example/mobile_app/MainActivity.kt
@@ -1,5 +1,51 @@
-package com.example.mobile_app
+package com.example.sdmt_final
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.sdtpro/network"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "bindToWiFi") {
+                val success = bindToWiFi()
+                result.success(success)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun bindToWiFi(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val request = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    connectivityManager.bindProcessToNetwork(network)
+                } else {
+                    ConnectivityManager.setProcessDefaultNetwork(network)
+                }
+                connectivityManager.unregisterNetworkCallback(this)
+            }
+        }
+        
+        connectivityManager.requestNetwork(request, networkCallback)
+        return true // Vraćamo true jer je zahtjev poslan asinkrono
+    }
+}
