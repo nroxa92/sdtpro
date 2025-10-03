21e8af50118c9e23504a4ef75de581a89dc34034
Neven <nevenroksa@gmail.com>
2025-10-03 14:19:22 +0200

    3

--- mobile_app/lib/screens/can_errors_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-class CanErrorsScreen extends StatelessWidget { // IME KLASE JE CanErrorsScreen
-  const CanErrorsScreen({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('CAN Greške')));
-  }
-}
